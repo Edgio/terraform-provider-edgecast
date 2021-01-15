14300ba747126fb2f47ceb9b5fec04b45e57cf4b
Changgyu Oh <changgyu.oh@verizondigitalmedia.com>
2021-01-15 10:27:17 -0800

    need merge master before resume the work

---
+++ vmp/resource_rulesengine_v4policy.go
@@ -0,0 +1,133 @@
+// Copyright Verizon Media, Licensed under the terms of the Apache 2.0 license . See LICENSE file in project root for terms.
+
+package vmp
+
+import (
+	"context"
+	"strconv"
+
+	"terraform-provider-vmp/vmp/api"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceRulesEngineV4Policy() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourcePolicyCreate,
+		ReadContext:   resourcePolicyRead,
+		UpdateContext: resourcePolicyUpdate,
+		DeleteContext: resourcePolicyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"policy": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourcePolicyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	accountNumber := d.Get("account_number").(string)
+	providerConfiguration, err := m.(*ProviderConfiguration).ApplyAccountNumberOverride(accountNumber)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	addCnameRequest := &api.AddCnameRequest{
+		Name:        d.Get("name").(string),
+		MediaTypeId: d.Get("type").(int),
+		OriginId:    d.Get("origin_id").(int),
+		OriginType:  d.Get("origin_type").(int),
+	}
+
+	cnameAPIClient := api.NewCnameApiClient(providerConfiguration.APIClient, providerConfiguration.AccountNumber)
+
+	parsedResponse, err := cnameAPIClient.AddCname(addCnameRequest)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(parsedResponse.CnameId))
+
+	return resourceCnameRead(ctx, d, m)
+}
+
+func resourcePolicyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	providerConfiguration, err := m.(*ProviderConfiguration).ApplyAccountNumberOverride(d.Get("account_number").(string))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	cnameAPIClient := api.NewCnameApiClient(providerConfiguration.APIClient, providerConfiguration.AccountNumber)
+
+	cnameID, _ := strconv.Atoi(d.Id())
+
+	parsedResponse, err := cnameAPIClient.GetCname(cnameID)
+
+	if err != nil {
+		d.SetId("")
+		return diag.FromErr(err)
+	}
+
+	d.Set("name", parsedResponse.Name)
+	d.Set("origin_id", parsedResponse.OriginId)
+	d.Set("origin_string", parsedResponse.OriginString)
+
+	return diags
+}
+
+func resourcePolicyUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	providerConfiguration, err := m.(*ProviderConfiguration).ApplyAccountNumberOverride(d.Get("account_number").(string))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	addCnameRequest := &api.AddCnameRequest{
+		Name:        d.Get("name").(string),
+		MediaTypeId: d.Get("type").(int),
+		OriginId:    d.Get("origin_id").(int),
+		OriginType:  d.Get("origin_type").(int),
+	}
+
+	cnameAPIClient := api.NewCnameApiClient(providerConfiguration.APIClient, providerConfiguration.AccountNumber)
+
+	parsedResponse, err := cnameAPIClient.AddCname(addCnameRequest)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(parsedResponse.CnameId))
+
+	return resourceCnameRead(ctx, d, m)
+}
+
+func resourcePolicyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	providerConfiguration, err := m.(*ProviderConfiguration).ApplyAccountNumberOverride(d.Get("account_number").(string))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	cnameAPIClient := api.NewCnameApiClient(providerConfiguration.APIClient, providerConfiguration.AccountNumber)
+
+	cnameID, _ := strconv.Atoi(d.Id())
+
+	err = cnameAPIClient.DeleteCname(cnameID)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return diags
+}
