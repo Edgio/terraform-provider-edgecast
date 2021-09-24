37e006c7dde35fa224ae3c46a2e6eee4209b86be
Shikha Saluja <ssaluja@ad.ouroath.com>
2021-09-24 10:04:11 -0700

    added doc file, updated resource file to be more indepth

--- examples/resources/ec_waf_custom_rule_set/resources.tf
+++ examples/resources/ec_waf_custom_rule_set/resources.tf
@@ -1,9 +1,9 @@
 
 resource "ec_waf_custom_rule_set" "custom_rule_1" {
-  account_number = "ca5db"
+  account_number = "<account_number>"
   name           = "Custom Rule 1"
   
-  directive {
+directive {
       sec_rule {
           name = "Sec Rule 1"
           action {
@@ -39,7 +39,32 @@ resource "ec_waf_custom_rule_set" "custom_rule_1" {
               } 
               type = "REQUEST_URI"
           }
-          
+          chained_rule {
+                action {
+                id = 66000001 
+                msg = "Invalid user agent - chained."
+                t = ["NONE"]
+            }
+            operator {
+                is_negated = false
+                type = "CONTAINS"
+                value = "bot"
+            }
+            variable {
+                is_count = false
+                match {
+                    is_negated = false
+                    is_regex = false
+                    value = "User-Agent"
+                } 
+                match {
+                    is_negated = false
+                    is_regex = false
+                    value = "User-Agent"
+                }
+                type = "REQUEST_HEADERS"
+            }
+          }
         }
     }
 }
