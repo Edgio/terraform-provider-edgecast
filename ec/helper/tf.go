34c82e900ce06c08d95453facf859bf6d56c5631
Frank Contreras <frank.contreras@verizondigitalmedia.com>
2021-11-23 13:49:48 -0800

    Adding comma back to the copyright to match Go SDK.

--- ec/helper/tf.go
+++ ec/helper/tf.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Edgecast Inc. Licensed under the terms of the Apache 2.0 license.
+// Copyright 2021 Edgecast Inc., Licensed under the terms of the Apache 2.0 license.
 // See LICENSE file in project root for terms.
 package helper
 
