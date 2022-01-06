7a472b2450108a8d1f6b1e4da824df98eaa834ef
wdawg33 <frank.contreras@verizondigitalmedia.com>
2022-01-06 09:33:48 -0800

    Updated edgecname resource definition to use raw string literals per code review feedback to improve performance and readability. Regenerated resource markdown file post change.

--- docs/resources/edgecname.md
+++ docs/resources/edgecname.md
@@ -37,19 +37,46 @@ resource "ec_edgecname" "edgecname_customer_origin" {
 
 ### Required
 
-- **account_number** (String) Account Number for the customer if not already specified in the provider configuration.
-- **media_type_id** (Number) Identifies the Delivery Platform on which the edge CNAME will be created. 3:Http Large, 8:HTTP Small, 14: ADN
-- **name** (String) Sets the name that will be assigned to the edge CNAME. It should only contain lower-case alphanumeric characters, dashes, and periods. The name specified for this parameter should also be defined as a CNAME record on a DNS server. The CNAME record defined on the DNS server should point to the CDN hostname (e.g., wpc.0001.edgecastcdn.net) for the platform identified by the `platform` parameter
-- **origin_id** (Number) Identifies whether an edge CNAME will be created for a CDN origin server or a customer origin server. Valid values: -1 Indicates that you would like to create an edge CNAME for our CDN storage service, CustomerOriginID: Specifying an ID for an existing customer origin configuration indicates that you would like to create an edge CNAME for that customer origin
+- **account_number** (String) Account Number for the customer if not already
+					specified in the provider configuration.
+- **media_type_id** (Number) Identifies the Delivery Platform on which the
+					edge CNAME will be created. 
+					3:Http Large, 8:HTTP Small, 14: ADN
+- **name** (String) Sets the name that will be assigned to the edge
+					CNAME. It should only contain lower-case alphanumeric
+					characters, dashes, and periods. The name specified for
+					this parameter should also be defined as a CNAME record
+					on a DNS server. The CNAME record defined on the DNS server 
+					should point to the CDN hostname
+					(e.g., wpc.0001.edgecastcdn.net) for the platform
+					identified by the "platform" parameter
+- **origin_id** (Number) Identifies whether an edge CNAME will be created
+					for a CDN origin server or a customer origin server. 
+					Valid values: 
+					-1: Indicates that you would like to create an
+					edge CNAME for our CDN storage service,
+					CustomerOriginID: Specifying an ID for an existing
+					customer origin configuration indicates that you would
+					like to create an edge CNAME for that customer origin
 
 ### Optional
 
-- **dir_path** (String) Identifies a location on the origin server. This string should specify the relative path from the root folder of the origin server to the desired location. Set this parameter to blank to point the edge CNAME to the root folder of the origin server.
-- **enable_custom_reports** (Number) Determines whether hits and data transferred statistics will be tracked for this edge CNAME. Logged data can be viewed through the Custom Reports module. Valid values are:0: Disabled (Default Value).1: Enabled. CDN activity on this edge CNAME will be logged.
+- **dir_path** (String) Identifies a location on the origin server. This
+					string should specify the relative path from the root
+					folder of the origin server to the desired location. Set
+					this parameter to blank to point the edge CNAME to the
+					root folder of the origin server.
+- **enable_custom_reports** (Number) Determines whether hits and data transferred
+					statistics will be tracked for this edge CNAME. Logged
+					data can be viewed through the Custom Reports module.
+					Valid values are:
+					0: Disabled (Default Value).
+					1: Enabled. CDN activity on this edge CNAME will be logged.
 - **id** (String) The ID of this resource.
 
 ### Read-Only
 
-- **origin_string** (String) Indicates the origin identifier, the account number, and the relative path associated with the edge CNAME.
+- **origin_string** (String) Indicates the origin identifier, the account
+					number, and the relative path associated with the edge CNAME.
 
 
--- ec/resources/edgecname/edgecname.go
+++ ec/resources/edgecname/edgecname.go
@@ -26,63 +26,64 @@ func ResourceEdgeCname() *schema.Resource {
 			"account_number": {
 				Type:     schema.TypeString,
 				Required: true,
-				Description: "Account Number for the customer if not already " +
-					"specified in the provider configuration.",
+				Description: `Account Number for the customer if not already
+					specified in the provider configuration.`,
 			},
 			"name": {
 				Type:     schema.TypeString,
 				Required: true,
-				Description: "Sets the name that will be assigned to the edge " +
-					"CNAME. It should only contain lower-case alphanumeric " +
-					"characters, dashes, and periods. The name specified for " +
-					"this parameter should also be defined as a CNAME record " +
-					"on a DNS server. The CNAME record defined on the DNS " +
-					"server should point to the CDN hostname " +
-					"(e.g., wpc.0001.edgecastcdn.net) for the platform " +
-					"identified by the `platform` parameter",
+				Description: `Sets the name that will be assigned to the edge
+					CNAME. It should only contain lower-case alphanumeric
+					characters, dashes, and periods. The name specified for
+					this parameter should also be defined as a CNAME record
+					on a DNS server. The CNAME record defined on the DNS server 
+					should point to the CDN hostname
+					(e.g., wpc.0001.edgecastcdn.net) for the platform
+					identified by the "platform" parameter`,
 			},
 			"dir_path": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Description: "Identifies a location on the origin server. This " +
-					"string should specify the relative path from the root " +
-					"folder of the origin server to the desired location. Set " +
-					"this parameter to blank to point the edge CNAME to the " +
-					"root folder of the origin server.",
+				Description: `Identifies a location on the origin server. This
+					string should specify the relative path from the root
+					folder of the origin server to the desired location. Set
+					this parameter to blank to point the edge CNAME to the
+					root folder of the origin server.`,
 			},
 			"enable_custom_reports": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Description: "Determines whether hits and data transferred " +
-					"statistics will be tracked for this edge CNAME. Logged " +
-					"data can be viewed through the Custom Reports module. " +
-					"Valid values are:" +
-					"0: Disabled (Default Value)." +
-					"1: Enabled. CDN activity on this edge CNAME will be logged.",
+				Description: `Determines whether hits and data transferred
+					statistics will be tracked for this edge CNAME. Logged
+					data can be viewed through the Custom Reports module.
+					Valid values are:
+					0: Disabled (Default Value).
+					1: Enabled. CDN activity on this edge CNAME will be logged.`,
 			},
 			"media_type_id": {
 				Type:     schema.TypeInt,
 				Required: true,
-				Description: "Identifies the Delivery Platform on which the " +
-					"edge CNAME will be created. " +
-					"3:Http Large, 8:HTTP Small, 14: ADN",
+				Description: `Identifies the Delivery Platform on which the
+					edge CNAME will be created. 
+					3:Http Large, 8:HTTP Small, 14: ADN`,
 			},
 			"origin_id": {
 				Type:     schema.TypeInt,
 				Required: true,
-				Description: "Identifies whether an edge CNAME will be created " +
-					"for a CDN origin server or a customer origin server. Valid " +
-					"values: -1 Indicates that you would like to create an " +
-					"edge CNAME for our CDN storage service, " +
-					"CustomerOriginID: Specifying an ID for an existing " +
-					"customer origin configuration indicates that you would " +
-					"like to create an edge CNAME for that customer origin",
+				Description: `Identifies whether an edge CNAME will be created
+					for a CDN origin server or a customer origin server. 
+					Valid values: 
+					-1: Indicates that you would like to create an
+					edge CNAME for our CDN storage service,
+					CustomerOriginID: Specifying an ID for an existing
+					customer origin configuration indicates that you would
+					like to create an edge CNAME for that customer origin`,
 			},
 			"origin_string": {
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: "Indicates the origin identifier, the account " +
-					"number, and the relative path associated with the edge CNAME.",
+				Description: `Indicates the origin identifier, the account
+					number, and the relative path associated with the edge CNAME.`,
 			},
 		},
 	}
