3cab32809895a43d3e96768e608ee93565f86018
wdawg33 <frank.contreras@verizondigitalmedia.com>
2022-03-04 16:36:27 -0800

    Merging ec to edgecast name change

--- ec/resources/dnsroute/common.go
+++ ec/resources/dnsroute/common.go
@@ -4,7 +4,7 @@
 package dnsroute
 
 import (
-	"terraform-provider-ec/ec/api"
+	"terraform-provider-edgecast/ec/api"
 
 	"github.com/EdgeCast/ec-sdk-go/edgecast"
 	"github.com/EdgeCast/ec-sdk-go/edgecast/auth"
