f8e4f818772e41ec2d5620d55eedaa7f3a6f9819
Hector Gray <defstream@incrediMAC.local>
2022-05-10 13:24:11 -0700

    localizing the integration tests source

--- test/integration/resources/customer_user/main.tf
+++ test/integration/resources/customer_user/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     edgecast = {
       version = "0.5.1"
-      source  = "EdgeCast/edgecast"
+      source  = "github.com/terraform-providers/edgecast"
     }
   }
 }
