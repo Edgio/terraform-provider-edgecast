a93a56bccc287995a10f8f3695f03647f838c0ea
Steven Paz <steven@stevenpaz.com>
2023-02-21 19:41:36 -0800

    copyright year updates

--- edgecast/resources/waf_bot_manager/bot_manager_advanced.go
+++ edgecast/resources/waf_bot_manager/bot_manager_advanced.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Edgecast Inc., Licensed under the terms of the Apache 2.0
+// Copyright 2023 Edgecast Inc., Licensed under the terms of the Apache 2.0
 // license. See LICENSE file in project root for terms.
 
 package waf_bot_manager
