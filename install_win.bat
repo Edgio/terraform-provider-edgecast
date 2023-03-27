f2bfe0caac73942e5c7f52994e97082a423898bf
Shikha Saluja <shikha.c.saluja@gmail.com>
2023-03-27 16:14:03 -0700

    version bump

--- install_win.bat
+++ install_win.bat
@@ -9,7 +9,7 @@ set HOSTNAME=github.com
 set NAMESPACE=terraform-providers
 set NAME=edgecast
 set BINARY=terraform-provider-%NAME%.exe
-set VERSION=1.2.0
+set VERSION=1.2.1
 set OS_ARCH=windows_amd64
 
 go build
